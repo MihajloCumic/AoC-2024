fe53d43ec99217b466cfd47325711f24967520a1
MihajloCumic <mihajlo.cumic1999@gmail.com>
2024-12-12 20:03:22 +0100

    finished day 12 part 2 (need cleaning)

--- src/day12/Garden.java
+++ src/day12/Garden.java
@@ -3,10 +3,7 @@ package day12;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Garden {
 
@@ -30,6 +27,12 @@ public class Garden {
         public Pair(int row, int col) {
             this.row = row;
             this.col = col;
+        }
+        static void sortByRow(List<Pair> pairs){
+            pairs.sort(Comparator.comparingInt((Pair p) -> p.row).thenComparing(p -> p.col));
+        }
+        static void sortByCol(List<Pair> pairs){
+            pairs.sort(Comparator.comparingInt((Pair p) -> p.col).thenComparing(p -> p.row));
         }
     }
 }
